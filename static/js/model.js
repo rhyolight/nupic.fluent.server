81adb04b971c35b955d2f413636f34c0b9c92362
Chetan Surpur <chetan51@gmail.com>
2014-02-26 18:45:47 -0800

    Progress on UI

--- static/js/model.js
+++ static/js/model.js
@@ -1,1 +1,35 @@
-alert(window.MODEL_ID)
+/* Main */
+
+$("#input").focus();
+
+$("#input").keydown(function(e) {
+	if (e.which == 13) {  // enter
+		e.preventDefault();
+	}
+	else if (e.which == 32) {  // space
+		feed($(this).val())
+
+		$(this).val("")
+		e.preventDefault();
+	}
+	else if (e.which == 190) {  // period
+		alert('period');
+
+		e.preventDefault();
+	}
+});
+
+/* API functions */
+
+function feed(term) {
+	$("#history").append(buildHistoryRow(term));
+}
+
+/* Utility functions */
+
+function buildHistoryRow(term) {
+	return $("<tr id='term-" + term.hashCode() + "'><td class='term'>" +
+	          term + "</td><td class='prediction'>" +
+	          "<img src='/static/img/loading.gif' />" +
+	          "</td></tr>");
+}
